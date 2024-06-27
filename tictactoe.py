fa121e2bc4198ceb59219f82e53ec22318b4f8d7
Jack Lingle <captainjack0818@gmail.com>
2024-06-27 12:22:00 -0400

    add player, winner, actions functions

---
+++ tictactoe.py
@@ -0,0 +1,150 @@
+"""
+Tic Tac Toe Player
+"""
+import copy
+import math
+
+X = "X"
+O = "O"
+EMPTY = None
+
+
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+    """
+    x_count = 0
+    o_count = 0
+    for row in board:
+        for space in row:
+            if space == X:
+                x_count += 1
+            elif space == O:
+                o_count += 1
+
+    if x_count > o_count:
+        return O
+    return X
+
+
+def actions(board):
+    """
+    Returns set of all possible actions (i, j) available on the board.
+    """
+    possible_actions = set()
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == EMPTY:
+                possible_actions.add((i, j))
+    return possible_actions
+
+
+def result(board, action):
+    """
+    Returns the board that results from making move (i, j) on the board.
+    """
+    copiedBoard = copy.deepcopy(board)
+    if copiedBoard[action[0]][action[1]] == EMPTY:
+        if player(copiedBoard) == X:
+            copiedBoard[action[0]][action[1]] = X
+        else:
+            copiedBoard[action[0]][action[1]] = O
+        return copiedBoard
+    else:
+        raise Exception("Action not available")
+
+
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    """
+    o_win = False
+    x_win = False
+
+    # check for horizontal win
+    for row in board:
+        if row[0] == row[1] == row[2] == X:
+            x_win = True
+            return X
+        elif row[0] == row[1] == row[2] == O:
+            o_win = True
+            return O
+
+    # check for vertical win
+    for i in range(3):
+        if board[i][0] == X:
+            x_win = True
+            o_win = False
+        elif board[i][0] == O:
+            o_win = True
+            x_win = False
+        else:
+            x_win = False
+            o_win = False
+    if x_win:
+        return X
+    elif o_win:
+        return O
+
+    # check for left-right diagonal
+    for i in range(3):
+        if board[i][i] == X:
+            x_win = True
+            o_win = False
+        elif board[i][i] == O:
+            o_win = True
+            x_win = False
+        else:
+            x_win = False
+            o_win = False
+    if x_win:
+        return X
+    elif o_win:
+        return O
+
+    # check for right-left diagonal
+    for i in range(3):
+        for j in reversed(range(3)):
+            if board[i][j] == X:
+                x_win = True
+                o_win = False
+            elif board[i][j] == O:
+                o_win = True
+                x_win = False
+            else:
+                x_win = False
+                o_win = False
+    if x_win:
+        return X
+    elif o_win:
+        return O
+
+
+def terminal(board):
+    """
+    Returns True if game is over, False otherwise.
+    """
+    raise NotImplementedError
+
+
+def utility(board):
+    """
+    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    """
+    raise NotImplementedError
+
+
+def minimax(board):
+    """
+    Returns the optimal action for the current player on the board.
+    """
+    raise NotImplementedError
